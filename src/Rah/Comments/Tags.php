6e1e62be9b2e26c068a20334fb342403c355cc65
Jukka Svahn <web@rahinaa.biz>
2015-03-23 01:53:14 +0200

    Rewritten as etc_pagination module.

--- src/Rah/Comments/Tags.php
+++ src/Rah/Comments/Tags.php
@@ -1,104 +1,152 @@
 <?php
 
-	################
-	#
-	#	rah_comments-plugin for Textpattern
-	#	version 0.4
-	#	by Jukka Svahn
-	#	http://rahforum.biz
-	#
-	################
+/*
+ * rah_comments - Flat templates for Textpattern CMS
+ * https://github.com/gocom/rah_comments
+ *
+ * Copyright (C) 2015 Jukka Svahn
+ *
+ * This file is part of rah_comments.
+ *
+ * rah_comments is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, version 2.
+ *
+ * rah_comments is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with rah_comments. If not, see <http://www.gnu.org/licenses/>.
+ */
 
-	function rah_comments($atts) {
-		global $thisarticle;
-		extract(lAtts(array(
-			'form' => 'comments',
-			'sort' => 'posted asc',
-			'wraptag' => '',
-			'break' => '',
-			'class' => '',
-			'breakclass' => '',
-			'offset' => '',
-			'limit' => 10,
-			'prepend' => 1,
-			'append' => 1,
-			'pg_wraptag' => 'ul',
-			'pg_break' => 'li',
-			'pg_class' => '',
-			'pg_breakclass' => '',
-			'label_next' => '&gt;&gt;',
-			'label_prev' => '&lt;&lt;',
-			'class_next' => '',
-			'class_prev' => '',
-			'showalways' => 1,
-		),$atts));
-		$number = array();
-		if(is_numeric(gps('pg'))) $pg = gps('pg');
-		if(empty($pg)) $pg = 1;
-		$i = 0;
-		$prevpg = $pg-1;
-		$nextpg = $pg+1;
-		$next = '';
-		$prev = '';
-		$total = $thisarticle['comments_count'];
-		$totalpg = $total/$limit;
-		$totalpg = ceil($totalpg);
-		$sep = (safe_field('val','txp_prefs',"name='permlink_mode'") == 'messy') ? '&amp;' : '?';
-		if(1<$pg && 1<$totalpg) $number[] = doTag($label_prev,'a',$class_prev,' href="'.permlink(array()).$sep.'pg='.$prevpg.'"');
-		while ($i<$totalpg) {
-			$i++;
-			$number[] = doTag($i,'a','page-'.$i,' href="'.permlink(array()).$sep.'pg='.$i.'"');
-		}
-		if($pg<$totalpg) $number[] = doTag($label_next,'a',$class_next,' href="'.permlink(array()).$sep.'pg='.$nextpg.'"');
-		$pagination = ($showalways != 1 && $totalpg < 2) ? '' : doWrap($number, $pg_wraptag, $pg_break, $pg_class, $pg_breakclass);
-		if(1<$pg){
-			$offset = $limit*$pg;
-			$offset = $offset-$limit;
-		}
-		return (($prepend) ? $pagination : '').comments(array(
-			'sort' => $sort,
-			'form' => $form,
-			'wraptag' => $wraptag,
-			'breakclass' => $breakclass,
-			'break' => $break,
-			'class' => $class,
-			'offset' => $offset,
-			'limit' => $limit
-		)).(($append) ? $pagination : '');
-	}
+/**
+ * Returns number of comment pages.
+ *
+ * Number of pages is calculated based on
+ * the provided offset and limit attributes.
+ * The resulting value should be passed to
+ * etc_pagination or similar pagination generator.
+ *
+ * <code>
+ * <txp:variable name="numPages" value='<txp:rah_comments_numpages limit="10" />' />
+ * <txp:etc_pagination pages='<txp:variable name="numPages" />' />
+ * <txp:comments offset='<txp:etc_offset pageby="10" />' limit="10" />
+ * </code>
+ *
+ * This tag is also responsible of redirecting comment requests
+ * to the correct page.
+ *
+ * @param  array $atts Attributes
+ * @return int   Number of pages
+ */
 
-	function rah_recent_comments($atts) {
-		extract(lAtts(array(
-			'comments_limit' => 10,
-			'comments_sort' => 'posted asc',
-			'break' => 'br',
-			'class' => 'recent_comments',
-			'label' => '',
-			'labeltag' => '',
-			'limit' => 10,
-			'sort' => 'posted desc',
-			'wraptag' => ''
-		),$atts));
+function rah_comments_numpages($atts)
+{
+    global $etc_pagination_total, $thisarticle;
 
-		$sep = (safe_field('val','txp_prefs',"name='permlink_mode'") == 'messy') ? '&amp;' : '?';
-		$sort = preg_replace('/\bposted\b/', 'd.posted', $sort);
-		$rs = startRows('select d.name, d.parentid, d.discussid, t.ID as thisid, unix_timestamp(t.Posted) as posted, t.Title as title, t.Section as section, t.url_title '.'from '. safe_pfx('txp_discuss') .' as d inner join '. safe_pfx('textpattern') .' as t on d.parentid = t.ID '.'where t.Status >= 4 and d.visible = '.VISIBLE.' order by '.doSlash($sort).' limit 0,'.intval($limit));
+    extract(lAtts(array(
+        'limit'     => 10,
+        'offset'    => 0,
+        'sort'      => 'posted asc',
+        'parameter' => 'pg',
+    ), $atts));
 
-		if ($rs) {
-			$out = array();
-			while ($c = nextRow($rs)) {
-				$position = 0;
-				$pg = 0;	
-				$rs2 = safe_rows_start('discussid','txp_discuss',"visible = ".VISIBLE." and parentid = ".$c['parentid']." order by $comments_sort");
-				while ($x = nextRow($rs2)) {
-					extract($x);
-					$position++;
-					if($discussid == $c['discussid']) break;
-				}
-				$pg = ceil($position/$comments_limit);
-				$out[] = href($c['name'].' ('.escape_title($c['title']).')',permlinkurl($c).$sep.'pg='.$pg.'#c'.$c['discussid']);
-			}
-			if ($out) return doLabel($label, $labeltag).doWrap($out, $wraptag, $break, $class);
-		}
-		return '';
-	}
+    assert_article();
+
+    $limit = max(1, (int) $limit);
+    $offset = max(0, (int) $offset);
+
+    $commentId = (int) gps('rah_comments_id');
+    $commented = (int) gps('commented');
+
+    // If requesting a specific comment, find the proper page.
+
+    if ($commentId || $commented) {
+
+        $sql = array(
+            "visible = ".VISIBLE." and parentid = ".intval($thisarticle['thisid'])
+        );
+
+        if ($sort) {
+            $sql[] = 'order by '.doSlash($sort);
+        }
+
+        $sql[] = 'limit '.intval($offset).', '.PHP_INT_MAX;
+
+        $comments = safe_column(
+            'discussid',
+            'txp_discuss',
+            implode(' ', $sql)
+        );
+
+        $finalPosition = $position = $currentId = 0;
+
+        foreach ($comments as $comment) {
+            $position++;
+            $comment = (int) $comment;
+
+            if ($commentId === $comment) {
+                $finalPosition = $position;
+                break;
+            }
+
+            if ($commented && $comment >= $currentId) {
+                $currentId = $comment;
+                $finalPosition = $position;
+            }
+        }
+
+        if ($finalPosition) {
+            $page = ceil($finalPosition / $limit);
+            $_POST[$parameter] = $_GET[$parameter] = $page;
+        }
+
+        // Prevents etc_paginate from picking up the 'commented' parameter.
+
+        if ($commented) {
+            $_POST['commented'] = gps('commented');
+            unset($_GET['commented']);
+        }
+    }
+
+    $totalcomments = max(0, $thisarticle['comments_count'] - $offset);
+    $totalpages = ceil($totalcomments / $limit);
+    $etc_pagination_total = $totalcomments;
+
+    return $totalpages;
+}
+
+/**
+ * Renders list of recent comments.
+ *
+ * @param  array  $atts  Attributes
+ * @param  string $thing Contained statement
+ * @return string User markup
+ */
+
+function rah_recent_comments($atts, $thing = null)
+{
+    $out = recent_comments($atts, $thing);
+
+    $out = preg_replace_callback(
+        '/(<a[^>]+href=")([^"#]+)#c([0-9]+)(")/i',
+
+        function ($m) {
+
+            if (strpos($m[2], '?') === false) {
+                $sep = '?';
+            } else {
+                $sep = '&amp;';
+            }
+
+            return $m[1].$m[2].
+                $sep.'rah_comments_id='.$m[3].'#c'.$m[3].$m[4];
+        },
+
+        $out
+    );
+
+    return $out;
+}
